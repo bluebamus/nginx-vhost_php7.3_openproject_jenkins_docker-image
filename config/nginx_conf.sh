4d0cbef3448d7d4f7dff34af0a036fa084ee9c42
bluebamus <bluebamus@naver.com>
2020-03-03 00:31:31 +0900

    update nginx site config files from conf.d to sites-available

--- config/nginx_conf.sh
+++ config/nginx_conf.sh
@@ -8,6 +8,6 @@ phpport=$4
 sed 's/account/'$account'/' sample_nginx.conf > $account'1'.temp
 sed 's/domain/'$domain'/g' $account'1'.temp > $account'2'.temp
 sed 's/portnumber;/'$portnumber';/' $account'2'.temp > $account'3'.temp
-sed 's/phpport/'$phpport'/' $account'3'.temp > ./conf.d/$account'_ng'.conf
+sed 's/phpport/'$phpport'/' $account'3'.temp > ./sites-available/$account'_ng'.conf
 
 rm *.temp
