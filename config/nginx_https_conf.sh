0f39d0f4bbe3cce80769e369dd04d28a03430a79
bluebamus <bluebamus@naver.com>
2020-03-06 00:06:27 +0900

    session cash change from 10M to 20M

--- config/nginx_https_conf.sh
+++ config/nginx_https_conf.sh
@@ -5,9 +5,9 @@ domain=$2
 portnumber=$3
 service_port=$4
 
-sed 's/account/'$account'/' sample_nginx.conf > $account'1'.temp
+sed 's/account/'$account'/' sample_nginx_https.conf > $account'1'.temp
 sed 's/domain/'$domain'/g' $account'1'.temp > $account'2'.temp
 sed 's/portnumber;/'$portnumber';/' $account'2'.temp > $account'3'.temp
-sed 's/service_port/'$service_port'/' $account'3'.temp > ./sites-available/$account'_ng'.conf 
+sed 's/service_port/'$service_port'/' $account'3'.temp > ./sites-available/$account'_https_ng'.conf 
 
 rm *.temp
